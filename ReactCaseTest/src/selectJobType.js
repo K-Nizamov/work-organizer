38518227a7aff12baef030331116293f17dcc50d
K-Nizamov <87225689+K-Nizamov@users.noreply.github.com>
2021-10-29 18:09:27 +0300

    Delete ReactCaseTest directory

--- ReactCaseTest/src/selectJobType.js
+++
@@ -1,8 +0,0 @@
-export function selectedTypeOfJob(menuReference) {
-
-    let jobTypeSelectIndex = menuReference.options.selectedIndex
-
-    let selectedType = menuReference[jobTypeSelectIndex].value;
-
-    return selectedType
-}
